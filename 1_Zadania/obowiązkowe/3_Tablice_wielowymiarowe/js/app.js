a36c76d761d2b91868fe7ffb91a5d9d306d52bac
Mateusz Sowiński <msowinski@sevillemore.com>
2019-10-08 22:20:12 +0200

    Fix for task about tables

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -12,4 +12,21 @@ var task2Array = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 10, 11, 12]
-];
+
+    function Create2DArray(rows, columns) {
+        let array = {};
+        let number = {0};
+        for(let i = 0; i < rows; i++){
+            let temp = {};
+            for(let j = 0; j < columns; j ++){
+                temp.push(number);
+                number++;
+
+
+            }
+            array.push(temp);
+        }
+        return array;
+    }
+
+    create2DArray(4,4);
